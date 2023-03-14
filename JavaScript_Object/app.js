833fca15d9112b50365aa72c1c639b84db9008c6
Niraj018 <noisyking7@gmail.com>
2023-03-14 22:21:52 +0545

    progress on Mp3 app

---
+++ JavaScript_Object/app.js
@@ -0,0 +1,31 @@
+
+
+var prev = document.querySelector("#prev");
+var next = document.querySelector("#next");
+
+var music = [{
+    writer: "Eminem",
+    Singer: "Eminem",
+    duration: 190,
+    Title: "Rap God"
+},{
+    writer: "50Cent",
+    singer: "50Cent",
+    duration: 220,
+    Title: "In Da club"
+},
+{
+    writer: "2Pac",
+    singer: "2Pac",
+    duration: 190,
+    Title: "Until I die"
+}]
+    
+     console.log(music[2].duration);
+
+     function start(){
+        
+     }
+
+    //  Map,filter,reduce  learn for today
+    
